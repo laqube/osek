25c992fc681ec7a89db2ae5d5b96fc07df79d08b
Miras <91594617+laqube@users.noreply.github.com>
2022-05-06 13:20:29 +0600

    test03
    the grid of main page is set up, next comes the falling header menu and registration/login forms

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import logo from '../logo.svg'
+import logoMobile from '../logoMobile.svg'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles";
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        alignContent: "center",
+        alignItems: "center",
+        ['@media (max-width:780px)']: {
+            flexDirection: "column"
+        }
+    },
+    logo: {
+        width: "15%",
+        ['@media (max-width:780px)']: {
+            display: "none"
+        }
+    },
+    logoMobile:{
+        width: "100%",
+        display: "none",
+        ['@media (max-width:780px)']: {
+            display: "inline-block"
+        }
+    },
+    menuItem: {
+        cursor: "pointer",
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: {
+            paddingBottom: "1rem"    }
+    }
+})
+
+function NavBar() {
+    const classes = styles()
+    return(
+        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" calssName={classes.bar}>
+            <img src={logo} className={classes.logo} alt="{}"/>
+            <img src={logoMobile} className={classes.logoMobile} alt="{}"/>
+            <Typography variant="h6" className={classes.menuItem}>
+                Juwirda
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Erlerge
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Bikelerge
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Balalarg'a
+            </Typography>
+            <CustomBtn txt="Tirkeluw" />
+        </Toolbar>
+    )
+}
+
+export default NavBar
