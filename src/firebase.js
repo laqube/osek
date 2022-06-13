a3705861e66d18f3284c0d4407a2527bc3f9421e
AB0s <93539709+AB0s@users.noreply.github.com>
2022-06-14 00:57:06 +0600

    Shop

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {getFirestore} from '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtP8mzM7rc2HOrGDIs5LQYFxxeSpRilPQ",
-    authDomain: "o-sek-d9a9f.firebaseapp.com",
-    projectId: "o-sek-d9a9f",
-    storageBucket: "o-sek-d9a9f.appspot.com",
-    messagingSenderId: "487189114907",
-    appId: "1:487189114907:web:85b8658d88adced04b48be"
+  apiKey: "AIzaSyAabA5NavbKGGX67w7SCRSXn5P8k5xeA7Y",
+  authDomain: "osek2-42d17.firebaseapp.com",
+  projectId: "osek2-42d17",
+  storageBucket: "osek2-42d17.appspot.com",
+  messagingSenderId: "698045052356",
+  appId: "1:698045052356:web:30f283cac149535b49e091"
   };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db=getFirestore(app);
